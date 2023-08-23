67a8d05ccc63d11b304b5e47fb2301bb1b279f78
SerhiiShymko <shimko456@gmail.com>
2023-08-24 00:08:39 +0300

    package

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
-// import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <BrowserRouter basename="/Goose-Track-06"> */}
     <App />
-    {/* </BrowserRouter> */}
   </React.StrictMode>
 );
