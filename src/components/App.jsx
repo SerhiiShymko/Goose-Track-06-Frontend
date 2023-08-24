124bc0514aa18fba28d2c394d120ddb3a4636607
SerhiiShymko <shimko456@gmail.com>
2023-08-24 17:38:20 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,17 @@
-import { lazy, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { selectToken } from 'redux/auth/selectors';
+// import { lazy, useEffect } from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { selectToken } from 'redux/auth/selectors';
 import { Container } from 'styles/Container';
 
-const MainPage = lazy(() => import('pages/MainPage/MainPage'));
-const AccountPage = lazy(() => import('pages/AccountPage/AccountPage'));
-const RegisterPage = lazy(() => import('pages/RegisterPage/RegisterPage'));
-const LoginPage = lazy(() => import('pages/LoginPage/LoginPage'));
-const CalendarPage = lazy(() => import('pages/CalendarPage/CalendarPage'));
-const NotFoundPage = lazy(() => import('pages/NotFoundPage/NotFoundPage'));
-const StatisticsPage = lazy(() =>
-  import('pages/StatisticsPage/StatisticsPage')
-);
+// const MainPage = lazy(() => import('pages/MainPage/MainPage'));
+// const AccountPage = lazy(() => import('pages/AccountPage/AccountPage'));
+// const RegisterPage = lazy(() => import('pages/RegisterPage/RegisterPage'));
+// const LoginPage = lazy(() => import('pages/LoginPage/LoginPage'));
+// const CalendarPage = lazy(() => import('pages/CalendarPage/CalendarPage'));
+// const NotFoundPage = lazy(() => import('pages/NotFoundPage/NotFoundPage'));
+// const StatisticsPage = lazy(() =>
+//   import('pages/StatisticsPage/StatisticsPage')
+// );
 
 export const App = () => {
   // const dispatch = useDispatch();
