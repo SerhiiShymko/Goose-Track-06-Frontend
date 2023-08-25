f1a11b3b0fac4a6701c9548f11156a356363c40c
SerhiiShymko <shimko456@gmail.com>
2023-08-25 11:47:22 +0300

    index-BR

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,23 @@
-// import { lazy, useEffect } from 'react';
+import { Suspense, lazy } from 'react';
+// import { refresh } from 'redux/auth/operations';
 // import { useDispatch, useSelector } from 'react-redux';
 // import { selectToken } from 'redux/auth/selectors';
-import { Container } from 'styles/Container';
 
-// const MainPage = lazy(() => import('pages/MainPage/MainPage'));
+import { Container } from 'styles/Container';
+import Spinner from './Spinner/Spinner';
+import { Route, Routes } from 'react-router-dom';
+
+const MainPage = lazy(() => import('pages/MainPage/MainPage'));
 // const AccountPage = lazy(() => import('pages/AccountPage/AccountPage'));
-// const RegisterPage = lazy(() => import('pages/RegisterPage/RegisterPage'));
-// const LoginPage = lazy(() => import('pages/LoginPage/LoginPage'));
+const RegisterPage = lazy(() => import('pages/RegisterPage/RegisterPage'));
+const LoginPage = lazy(() => import('pages/LoginPage/LoginPage'));
 // const CalendarPage = lazy(() => import('pages/CalendarPage/CalendarPage'));
-// const NotFoundPage = lazy(() => import('pages/NotFoundPage/NotFoundPage'));
+const NotFoundPage = lazy(() => import('pages/NotFoundPage/NotFoundPage'));
 // const StatisticsPage = lazy(() =>
 //   import('pages/StatisticsPage/StatisticsPage')
 // );
 
-export const App = () => {
+export function App() {
   // const dispatch = useDispatch();
   // const token = useSelector(selectToken);
 
@@ -23,18 +27,15 @@ export const App = () => {
 
   return (
     <Container>
-      <div
-        style={{
-          height: '100vh',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          fontSize: 40,
-          color: '#010101',
-        }}
-      >
-        APP
-      </div>
+      <Suspense fallback={<Spinner />}>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="login" element={<LoginPage />} />
+          <Route path="register" element={<RegisterPage />} />
+
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Suspense>
     </Container>
   );
-};
+}
