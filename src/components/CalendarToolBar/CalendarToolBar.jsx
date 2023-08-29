c20153c78dff71935e228fa14e90ddc87f0f8708
Jigsaw28 <levenko7@gmail.com>
2023-08-30 02:33:06 +0300

    created modal calendar

--- src/components/CalendarToolBar/CalendarToolBar.jsx
+++ src/components/CalendarToolBar/CalendarToolBar.jsx
@@ -6,61 +6,86 @@ import {
   eachDayOfInterval,
   endOfWeek,
   addDays,
+  subMonths,
+  addMonths,
+  parseISO,
 } from 'date-fns';
 
 import {
   ModalCalendar,
   Caption,
-  ListItem,
+  Button,
   WeekDays,
   Day,
-  LineBlock
+  DataText,
+  CalendarTable,
+  CalendarDate,
+  ActiveCalendarDate
 } from './CalendarToolBar.styled';
 import { Container } from 'styles/Container';
 import Prev from '../../images/calendar/chevron-left.svg';
 import Next from '../../images/calendar/chevron-right.svg';
-import Line from '../../images/calendar/Separator.svg'
+import Line from '../../images/calendar/Separator.svg';
 import { nanoid } from 'nanoid';
+import { useState } from 'react';
 
 export const CalendarToolbar = () => {
   const date = format(new Date(), 'MMMM yyyy');
+  const [selectedDate, setSelectedDate] = useState(date);
+  const [activeDate, setActiveDate] = useState(date);
+
   const startDayOfMonth = startOfMonth(new Date());
   const startDayOfWeek = startOfWeek(new Date(), { weekStartsOn: 1 });
   const endDayOfMonth = endOfMonth(new Date());
   const endDayOfWeek = endOfWeek(new Date(), { weekStartsOn: 1 });
-
-  const result = eachDayOfInterval({
-    start: startDayOfWeek,
-    end: endDayOfWeek,
-  });
+ 
 
   const weekDays = [];
   for (let day = 0; day < 7; day++) {
     weekDays.push(format(addDays(startDayOfWeek, day), 'EEEEE'));
   }
 
+ const result = eachDayOfInterval({
+    start: startDayOfMonth,
+    end: endDayOfMonth,
+ });
+
   return (
     <Container>
-      <button type="button">{date}</button>
+      <button type="button">{activeDate}</button>
       <ModalCalendar>
         <Caption>
-          <ListItem>
+          <Button
+            onClick={() =>
+              setActiveDate(format(subMonths(new Date(), 1), 'MMMM yyyy'))
+            }
+          >
             <img src={Prev} alt="prev" width={18} height={18} />
-          </ListItem>
-          <li>{date}</li>
-          <ListItem>
+          </Button>
+          <DataText>{activeDate}</DataText>
+          <Button
+            onClick={() =>
+              setActiveDate(format(addMonths(new Date(), 1), 'MMMM yyyy'))
+            }
+          >
             <img src={Next} alt="next" width={18} height={18} />
-          </ListItem>
+          </Button>
         </Caption>
         <WeekDays>
           {weekDays.map(day => (
-            <Day key ={nanoid()}>{day}</Day>
+            <Day key={nanoid()}>{day}</Day>
           ))}
         </WeekDays>
-        <LineBlock>
-          <img src={Line} alt="line" width={328} height={1}/>
-        </LineBlock>
+        <CalendarTable>
+          {result.map(day => {
+            if (format(day, 'd MMMM') === format(new Date(), 'd MMMM')) {
+
+             return <ActiveCalendarDate key={day.toString()}>{format(day, 'd')}</ActiveCalendarDate>
+            }
+            return <CalendarDate key={day.toString()}>{format(day, 'd')}</CalendarDate>
+          })}
+        </CalendarTable>
       </ModalCalendar>
-      </Container>
+    </Container>
   );
 };
--- src/components/CalendarToolBar/CalendarToolBar.styled.jsx
+++ src/components/CalendarToolBar/CalendarToolBar.styled.jsx
@@ -8,14 +8,29 @@ export const ModalCalendar = styled.div`
   border-radius: 16px;
 `;
 
-export const Caption = styled.ul`
-  
+export const Caption = styled.div`
+  list-style: none;
   padding-inline-start: 0;
   display: flex;
   justify-content: space-between;
   align-items: center;
   margin: 0;
   color: #fff;
+`;
+
+export const Button = styled.button`
+  align-self: end; 
+  width: 18px;
+  height: 18px;
+  padding: 0;
+  background-color: inherit;
+  outline: none;
+  border: 0px;
+  cursor: pointer;
+`;
+
+export const DataText = styled.div`
+  padding: 4px 0;
   text-align: center;
   font-family: Inter;
   font-size: 20px;
@@ -24,21 +39,14 @@ export const Caption = styled.ul`
   line-height: 1.2;
 `;
 
-export const ListItem = styled.li`
-  width: 18px;
-  height: 18px;
-  align-self: end;
-  cursor: pointer;
-`;
-
 export const WeekDays = styled.div`
-  display: flex;
-  justify-content: space-around;
-
+  padding: 12px 0;
+  display: grid;
+  grid-template-columns: repeat(7, 1fr);
+  border-bottom: 1px solid rgba(255, 255, 255, 0.20);
 `;
-export const Day = styled.div`
-  padding: 12px 16px;
 
+export const Day = styled.div`
   color: #fff;
   text-align: center;
   font-family: Inter;
@@ -48,7 +56,31 @@ export const Day = styled.div`
   line-height: 1.28;
 `;
 
-export const LineBlock = styled.div`
-width: 328px;
-font-size: 1px;
+export const CalendarTable = styled.div`
+display: grid;
+grid-template-columns: repeat(7, 1fr); 
+grid-template-rows: repeat(5, 1fr);
+grid-row-gap: 3px;
+`
+export const CalendarDate = styled.div`
+padding: 12px 0;
+color: #FFF;
+text-align: center;
+font-family: Inter;
+font-size: 14px;
+font-style: normal;
+font-weight: 400;
+line-height: 18px; /* 128.571% */
+`
+export const ActiveCalendarDate = styled.div`
+padding: 12px 0;
+color: #3E85F3;
+text-align: center;
+font-family: Inter;
+font-size: 14px;
+font-style: normal;
+font-weight: 400;
+line-height: 1.28;
+border-radius: 50%;
+background-color: #fff;
 `
