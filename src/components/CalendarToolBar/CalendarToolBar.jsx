194d63774ccf6f84bef5f98bc35a71fad9598f41
Jigsaw28 <levenko7@gmail.com>
2023-08-29 21:23:38 +0300

    update

--- src/components/CalendarToolBar/CalendarToolBar.jsx
+++ src/components/CalendarToolBar/CalendarToolBar.jsx
@@ -1,26 +1,66 @@
-import { format, add } from 'date-fns';
+import {
+  endOfMonth,
+  format,
+  startOfMonth,
+  startOfWeek,
+  eachDayOfInterval,
+  endOfWeek,
+  addDays,
+} from 'date-fns';
 
-import { ModalCalendar, Caption, ListItem } from './CalendarToolBar.styled';
+import {
+  ModalCalendar,
+  Caption,
+  ListItem,
+  WeekDays,
+  Day,
+  LineBlock
+} from './CalendarToolBar.styled';
 import { Container } from 'styles/Container';
 import Prev from '../../images/calendar/chevron-left.svg';
 import Next from '../../images/calendar/chevron-right.svg';
+import Line from '../../images/calendar/Separator.svg'
+import { nanoid } from 'nanoid';
 
 export const CalendarToolbar = () => {
   const date = format(new Date(), 'MMMM yyyy');
+  const startDayOfMonth = startOfMonth(new Date());
+  const startDayOfWeek = startOfWeek(new Date(), { weekStartsOn: 1 });
+  const endDayOfMonth = endOfMonth(new Date());
+  const endDayOfWeek = endOfWeek(new Date(), { weekStartsOn: 1 });
+
+  const result = eachDayOfInterval({
+    start: startDayOfWeek,
+    end: endDayOfWeek,
+  });
+
+  const weekDays = [];
+  for (let day = 0; day < 7; day++) {
+    weekDays.push(format(addDays(startDayOfWeek, day), 'EEEEE'));
+  }
+
   return (
     <Container>
       <button type="button">{date}</button>
       <ModalCalendar>
         <Caption>
           <ListItem>
-              <img src={Prev} alt="prev" width={18} height={18} />
+            <img src={Prev} alt="prev" width={18} height={18} />
           </ListItem>
           <li>{date}</li>
           <ListItem>
             <img src={Next} alt="next" width={18} height={18} />
           </ListItem>
         </Caption>
+        <WeekDays>
+          {weekDays.map(day => (
+            <Day key ={nanoid()}>{day}</Day>
+          ))}
+        </WeekDays>
+        <LineBlock>
+          <img src={Line} alt="line" width={328} height={1}/>
+        </LineBlock>
       </ModalCalendar>
-    </Container>
+      </Container>
   );
 };
--- src/components/CalendarToolBar/CalendarToolBar.styled.jsx
+++ src/components/CalendarToolBar/CalendarToolBar.styled.jsx
@@ -2,22 +2,19 @@ import { styled } from 'styled-components';
 
 export const ModalCalendar = styled.div`
   background-color: #3e85f3;
-  max-width: 327px;
+  width: 327px;
   height: 354px;
   padding: 9px 18px;
   border-radius: 16px;
 `;
 
 export const Caption = styled.ul`
-  list-style: none;
-  width: 291px;
-  height: 32px;
-  padding-inline-start: 0;
-  margin: 0 auto;
   
+  padding-inline-start: 0;
   display: flex;
   justify-content: space-between;
   align-items: center;
+  margin: 0;
   color: #fff;
   text-align: center;
   font-family: Inter;
@@ -33,3 +30,25 @@ export const ListItem = styled.li`
   align-self: end;
   cursor: pointer;
 `;
+
+export const WeekDays = styled.div`
+  display: flex;
+  justify-content: space-around;
+
+`;
+export const Day = styled.div`
+  padding: 12px 16px;
+
+  color: #fff;
+  text-align: center;
+  font-family: Inter;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 1.28;
+`;
+
+export const LineBlock = styled.div`
+width: 328px;
+font-size: 1px;
+`
