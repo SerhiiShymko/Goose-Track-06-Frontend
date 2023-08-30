ae5e7422b2d74f9c68e49c6971d0e940b9d87e50
SerhiiShymko <shimko456@gmail.com>
2023-08-30 09:15:05 +0300

    AuthNavigate

--- src/components/AuthNavigate/AuthNavigate.jsx
+++ src/components/AuthNavigate/AuthNavigate.jsx
@@ -1,5 +1,16 @@
-import { Background } from './AuthNavigate.styled';
-//
-export const AuthNavigate = () => {
-  return <Background></Background>;
+import React from 'react';
+import PropTypes from 'prop-types';
+import { StyledAuthNavigate } from './AuthNavigate.styled';
+
+export const AuthNavigate = ({ link, text }) => {
+  return (
+    <div>
+      <StyledAuthNavigate to={link}>{text}</StyledAuthNavigate>
+    </div>
+  );
+};
+
+AuthNavigate.PropTypes = {
+  link: PropTypes.string.isRequired,
+  test: PropTypes.string.isRequired,
 };
