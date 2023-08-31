e12bcc141897dc319e49a4d272737fbee353988f
Maria Vovchuk <mari4ka62@gmail.com>
2023-08-31 19:27:57 +0300

    slider wirh swiper react

--- src/components/ReviewsSlider/ReviewsSlider.jsx
+++ src/components/ReviewsSlider/ReviewsSlider.jsx
@@ -1,116 +1,107 @@
-// import React from 'react';
-
-// import { Stars } from './Stars';
-// import {
-//   // ArrowLeft,
-//   // ArrowRight,
-//   // ColorContainer,
-//   // Comment,
-//   // Image,
-//   // ImgWrapper,
-//   // MainContainerSlider,
-//   // Name,
-//   // ReviewsTitle,
-//   // SliderContainer,
-//   SliderDescription,
-//   SliderImage,
-//   SliderMenuContainer,
-//   SliderMenuHeader,
-//   SliderMenuLeftSlider,
-//   SliderMenuProfileWrapper,
-//   SliderMenuQuoteIcon,
-//   SliderMenuRightSlider,
-//   SliderName,
-//   // Svg,
-//   Wrapper,
-// } from './ReviewsSlider.styled';
-// // import { useSelector } from 'react-redux';
-// // import { selectReviews } from 'redux/reviews/selectors';
-
-// import { useState } from 'react';
-// import { useEffect } from 'react';
-
-// import { reviews } from 'data/reviews_example';
-
-// function ReviewsSlider(props) {
-//   // const reviews = useSelector(selectReviews);
-
-//   const [currentIndex, setCurrentIndex] = useState(0);
-
-//   const handleClick = action => {
-//     const len = reviews.length;
-
-//     if (action === 'previous') {
-//       if (currentIndex - 1 === -1) {
-//         setCurrentIndex(len - 1);
-//       } else {
-//         setCurrentIndex(currentIndex - 1);
-//       }
-//     }
-
-//     if (action === 'future') {
-//       if (currentIndex + 1 === len) {
-//         setCurrentIndex(0);
-//       } else {
-//         setCurrentIndex(currentIndex + 1);
-//       }
-//     }
-//   };
-
-//   useEffect(() => {
-//     const myInterval = setInterval(() => {
-//       handleClick('future');
-//     }, 4000);
-
-//     return () => clearInterval(myInterval);
-//     // eslint-disable-next-line react-hooks/exhaustive-deps
-//   }, [currentIndex]);
-
-//   return (
-//     <>
-//       <SliderMenuContainer>
-//         <SliderMenuHeader>Reviews</SliderMenuHeader>
-//         <Wrapper>
-//           {reviews.map((person, index) => {
-//             return (
-//               <SliderMenuProfileWrapper
-//                 className={(() => {
-//                   const len = reviews.length;
-
-//                   if (currentIndex === 0 && index === len - 1) {
-//                     return 'previous';
-//                   } else if (currentIndex === len - 1 && index === 0) {
-//                     return 'future';
-//                   } else if (index === currentIndex) {
-//                     return 'current';
-//                   } else if (currentIndex > index) {
-//                     return 'previous';
-//                   } else {
-//                     return 'future';
-//                   }
-//                 })()}
-//                 key={person.owner._id}
-//               >
-//                 <SliderImage src={person.owner.avatarURL} />
-//                 <SliderName>{person.owner.name}</SliderName>
-//                 <Stars rating={person.rating} />
-//                 <SliderDescription>{person.comment}</SliderDescription>
-//               </SliderMenuProfileWrapper>
-//             );
-//           })}
-//         </Wrapper>
-//         <SliderMenuQuoteIcon />
-//         <SliderMenuLeftSlider onClick={() => handleClick('previous')} />
-//         <SliderMenuRightSlider onClick={() => handleClick('future')} />
-//       </SliderMenuContainer>
-//     </>
-//   );
-// }
-// export default ReviewsSlider;
 import React from 'react';
 
-const ReviewsSlider = () => {
-  return <div>ReviewsSlider</div>;
-};
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore from 'swiper';
+import { Navigation, Autoplay } from 'swiper/modules';
 
+import 'swiper/css';
+import 'swiper/css/virtual';
+import 'swiper/css/navigation';
+
+import { ReactComponent as LeftArrow } from '../../images/mainPage/leftArrow.svg';
+import { ReactComponent as RightArrow } from '../../images/mainPage/rightArrow.svg';
+
+import { Stars } from './Stars';
+
+import {
+  Container,
+  Title,
+  AvatarWrapper,
+  AvatarImg,
+  Name,
+  SliderCard,
+  ReviewText,
+  SwiperNavBox,
+  SwiperNavBtn,
+  UserInfo,
+  ReviewContainer,
+} from './ReviewsSlider.styled';
+
+// import { fetchReviews } from 'redux/reviews/operations';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { selectReviews } from 'redux/reviews/selectors';
+
+import { reviews } from 'data/reviews_example';
+
+const ReviewsSlider = () => {
+  // const dispatch = useDispatch();
+  // const reviews = useSelector(selectReviews);
+
+  // useEffect(() => {
+  //   dispatch(fetchReviews({ page: 1, limit: 8 }));
+  // }, [dispatch]);
+
+  const slides = reviews;
+
+  SwiperCore.use([Autoplay]);
+
+  return (
+    <section>
+      <Container>
+        <Title>Reviews</Title>
+        <div>
+          <Swiper
+            initialSlide={1}
+            slidesPerView={1}
+            autoplay={{ delay: 4000 }}
+            navigation={{
+              prevEl: '#prev-button',
+              nextEl: '#next-button',
+            }}
+            modules={[Navigation]}
+            direction={'horizontal'}
+            autoHeight={true}
+            spaceBetween={24}
+            loop={true}
+            breakpoints={{
+              320: {
+                slidesPerView: 1,
+                slidesPerGroup: 1,
+              },
+              1440: {
+                slidesPerView: 2,
+                slidesPerGroup: 2,
+              },
+            }}
+          >
+            {slides.map(slide => (
+              <SwiperSlide key={slide.owner._id}>
+                <SliderCard>
+                  <ReviewContainer>
+                    <AvatarWrapper>
+                      <AvatarImg src={slide.owner.avatarURL} alt="UserAvatar" />
+                    </AvatarWrapper>
+                    <UserInfo>
+                      <Name>{slide.owner.name}</Name>
+                      <Stars rating={slide.rating} />
+                    </UserInfo>
+                  </ReviewContainer>
+                  <ReviewText>{slide.comment}</ReviewText>
+                </SliderCard>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+        <SwiperNavBox>
+          <SwiperNavBtn id="prev-button">
+            <LeftArrow />
+          </SwiperNavBtn>
+          <SwiperNavBtn id="next-button">
+            <RightArrow />
+          </SwiperNavBtn>
+        </SwiperNavBox>
+      </Container>
+    </section>
+  );
+};
 export default ReviewsSlider;
