6337e7a99ca4915e652991b983f8abc7431b1545
Oleg <olegbilas@gmail.com>
2023-08-31 16:47:25 +0300

    помилка Саспенс

--- src/styles/Container.js
+++ src/styles/Container.js
@@ -1,24 +1,19 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  // padding: 24px 20px;
-  // width: 100%;
+  max-width: 100vw;
+  padding: 24px 20px;
 
-  // padding: 24px 20px 24px 20px;
-  // width: 100%;
+  @media screen and (min-width: 375px) {
+    width: 375px;
+  }
 
-  // margin: 0 auto;
+  @media screen and (min-width: 768px) {
+    width: 768px;
+    padding: 24px 32px;
+  }
 
-  // @media screen and (min-width: 375px) {
-  //   width: 375px;
-  // }
-
-  // @media screen and (min-width: 768px) {
-  //   width: 768px;
-  //   padding: 25px 32px;
-  // }
-
-  // @media screen and (min-width: 1440px) {
-  //   width: 1440px;
-  // }
+  @media screen and (min-width: 1440px) {
+    width: 1440px;
+  }
 `;
