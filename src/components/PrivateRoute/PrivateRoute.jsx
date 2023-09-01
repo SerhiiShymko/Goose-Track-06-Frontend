a1104d1816650b7f3be39196fd591849c30972e9
Oleg <olegbilas@gmail.com>
2023-09-01 09:45:42 +0300

    Update PrivateRoute.jsx

--- src/components/PrivateRoute/PrivateRoute.jsx
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -7,5 +7,8 @@ export const PrivateRoute = ({ component, redirectTo = '/' }) => {
   const isRefreshing = useSelector(selectIsRefreshing);
   const shouldRedirect = !isLoggedIn && !isRefreshing;
 
+  console.log('isLoggedIn', isLoggedIn);
+  console.log('isRefreshing', isRefreshing);
+  console.log('shouldRedirect', shouldRedirect);
   return shouldRedirect ? <Navigate to={redirectTo} replace /> : component;
 };
