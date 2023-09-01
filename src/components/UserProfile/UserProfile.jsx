21fa4b3012b9dc3b8d56433e1821a5640437ca4a
danya-yablonskiy <danya.yablonskiy.00@gmail.com>
2023-09-01 03:25:52 +0300

    Update UserProfile.jsx

--- src/components/UserProfile/UserProfile.jsx
+++ src/components/UserProfile/UserProfile.jsx
@@ -1,4 +1,4 @@
-// import userAvatar from '../../images/profile/ph_user.png';
+import { useEffect, useState } from 'react';
 import {
   MainWrapper,
   PlusIcon,
@@ -15,33 +15,21 @@ import {
   BtnSaveChanges,
   UserSurnameMain,
 } from './UserProfile.styled';
+import { useDispatch } from 'react-redux';
+
+import { useSelector } from 'react-redux';
+import { selectUser } from 'redux/auth/selectors';
 
 const UserProfile = () => {
+  const dispatch = useDispatch();
+  const currentUserInfo = useSelector(selectUser);
+  const a = currentUserInfo.name.split(' ');
+  console.log(a[0]);
   return (
     <MainWrapper>
       <UserPhotoWrapper>
         <UserPhoto />
-        <PlusIcon
-          viewBox="0 0 24 24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <circle cx="12" cy="12" r="12" fill="#3E85F3" />
-          <path
-            d="M12 6.75V17.25"
-            stroke="white"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-          <path
-            d="M6.75 12H17.25"
-            stroke="white"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-        </PlusIcon>
+        <PlusIcon />
       </UserPhotoWrapper>
       <UserInfoWrapper>
         <UserNameMain>Nadia Doe</UserNameMain>
