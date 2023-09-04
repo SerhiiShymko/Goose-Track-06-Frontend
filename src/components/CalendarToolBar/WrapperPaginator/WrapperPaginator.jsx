3dcf29e31c8a5689886d6f010bfe34270d6c441c
Sergiy Kushnir <s1983kushnir@gmail.com>
2023-09-04 10:18:23 +0300

    Логіка працює за місяць та день

--- src/components/CalendarToolBar/WrapperPaginator/WrapperPaginator.jsx
+++ src/components/CalendarToolBar/WrapperPaginator/WrapperPaginator.jsx
@@ -5,27 +5,42 @@ import { useState } from "react";
 import Prev from '../../../images/calendar/chevron-left.svg'
 import Next from '../../../images/calendar/chevron-left.svg'
 
-export const WrapperPaginator = ({ dayInterval, onNext, onPrev, dateToday }) => {
-    const [open, setOpen] = useState(false);
-    return (
-        <WrapperPagination>
-        <ButtonForOpenCalendar
-          type="button"
-          onClick={() => {
-            setOpen(!open);
-          }}
-        >
-          {dateToday}
-        </ButtonForOpenCalendar>
-        {open && <CalendarModal onNext={onNext} onPrev={onPrev} dateToday={dateToday} dayInterval={dayInterval} />}
-        <PeriodPaginator>
-          <BtnPaginatorLeft onClick={onPrev}>
-            <ImgPaginatorPrev src={Prev} alt="prev" width={16} height={16} />
-          </BtnPaginatorLeft>
-          <BtnPaginatorRight onClick={onNext}>
-            <ImgPaginatorNext src={Next} alt="next" width={16} height={16} />
-          </BtnPaginatorRight>
-        </PeriodPaginator>
-      </WrapperPagination>
-    )
-}
+export const WrapperPaginator = ({
+  dayInterval,
+  onNext,
+  onPrev,
+  dateToday,
+  handleClick,
+}) => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <WrapperPagination>
+      <ButtonForOpenCalendar
+        type="button"
+        onClick={() => {
+          setOpen(!open);
+        }}
+      >
+        {dateToday}
+      </ButtonForOpenCalendar>
+      {open && (
+        <CalendarModal
+          onNext={onNext}
+          onPrev={onPrev}
+          dateToday={dateToday}
+          dayInterval={dayInterval}
+          handleClick={handleClick}
+        />
+      )}
+      <PeriodPaginator>
+        <BtnPaginatorLeft onClick={onPrev}>
+          <ImgPaginatorPrev src={Prev} alt="prev" width={16} height={16} />
+        </BtnPaginatorLeft>
+        <BtnPaginatorRight onClick={onNext}>
+          <ImgPaginatorNext src={Next} alt="next" width={16} height={16} />
+        </BtnPaginatorRight>
+      </PeriodPaginator>
+    </WrapperPagination>
+  );
+};
