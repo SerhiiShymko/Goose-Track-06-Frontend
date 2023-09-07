8550f985d31f7d725130c334a769beedf6e50118
Oleg Bilas <olegbilas@gmail.com>
2023-09-07 15:21:33 +0300

    скрол у тасках місячного календаря

--- src/components/ChoosedMonth/ChoosedMonth.styled.jsx
+++ src/components/ChoosedMonth/ChoosedMonth.styled.jsx
@@ -100,7 +100,7 @@ export const DayOfMonth = styled.div`
   padding: 4px 6px;
   margin-left: auto;
   margin-right: 2px;
-  margin-bottom: 8px;
+  margin-bottom: 6px;
   width: 20px;
   height: 22px;
   border-radius: 6px;
@@ -110,7 +110,6 @@ export const DayOfMonth = styled.div`
   @media screen and (min-width: 768px) {
     width: 27px;
     height: 26px;
-    margin-bottom: 10px;
   }
   @media screen and (min-width: 1440px) {
     margin-right: 6px;
