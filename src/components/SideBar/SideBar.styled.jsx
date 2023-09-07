8a77a49ba854e07621605c6ccee82ba682b29317
danya-yablonskiy <danya.yablonskiy.00@gmail.com>
2023-09-07 17:29:13 +0300

    Update SideBar.styled.jsx

--- src/components/SideBar/SideBar.styled.jsx
+++ src/components/SideBar/SideBar.styled.jsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const ContainerSideBar = styled.div`
   display: flex;
   flex-direction: column;
-  height: 104vh;
+  height: 100vh;
 
   width: 225px;
   padding: 24px 20px;
@@ -13,7 +13,6 @@ export const ContainerSideBar = styled.div`
   @media screen and (min-width: 768px) {
     width: 289px;
     padding: 24px 32px;
-    height: 104vh;
   }
   @media screen and (max-width: 1439px) {
     position: absolute;
@@ -29,8 +28,5 @@ export const ContainerSideBar = styled.div`
     }
     transition: transform 250ms cubic-bezier(0.4, 0, 0.2, 1),
       opacity 250ms cubic-bezier(0.4, 0, 0.2, 1);
-  }
-  @media screen and (min-width: 1440px) {
-    height: 108vh;
   }
 `;
