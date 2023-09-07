7e1b9df798c7b00c6f9e637f031d1ee08d1aaeee
danya-yablonskiy <danya.yablonskiy.00@gmail.com>
2023-09-07 17:28:14 +0300

    Update SideBar.styled.jsx

--- src/components/SideBar/SideBar.styled.jsx
+++ src/components/SideBar/SideBar.styled.jsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const ContainerSideBar = styled.div`
   display: flex;
   flex-direction: column;
-  height: 100vh;
+  height: 104vh;
 
   width: 225px;
   padding: 24px 20px;
@@ -13,6 +13,7 @@ export const ContainerSideBar = styled.div`
   @media screen and (min-width: 768px) {
     width: 289px;
     padding: 24px 32px;
+    height: 104vh;
   }
   @media screen and (max-width: 1439px) {
     position: absolute;
@@ -28,5 +29,8 @@ export const ContainerSideBar = styled.div`
     }
     transition: transform 250ms cubic-bezier(0.4, 0, 0.2, 1),
       opacity 250ms cubic-bezier(0.4, 0, 0.2, 1);
+  }
+  @media screen and (min-width: 1440px) {
+    height: 108vh;
   }
 `;
